c2f96d37863aa1e20a0abb5453a50fd0cd87631b
Xaron11 <me@xaron11.dev>
2026-08-20 21:57:27 +0200

    Update flake.nix and flake.lock

--- flake.nix
+++ flake.nix
@@ -18,10 +18,19 @@
           buildInputs = with pkgs; [
             nodejs
             openssl
+            vtsls
+            typescript-language-server
+            vscode-langservers-extracted
+            tailwindcss-language-server
+            prisma-engines
           ];
 
           shellHook = ''
             export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath [ pkgs.openssl pkgs.stdenv.cc.cc.lib ]}:$LD_LIBRARY_PATH"
+            export PRISMA_SCHEMA_ENGINE_BINARY="${pkgs.prisma-engines}/bin/schema-engine"
+            export PRISMA_QUERY_ENGINE_BINARY="${pkgs.prisma-engines}/bin/query-engine"
+            export PRISMA_QUERY_ENGINE_LIBRARY="${pkgs.prisma-engines}/lib/libquery_engine.so"
+            export PRISMA_FMT_BINARY="${pkgs.prisma-engines}/bin/prisma-fmt"
           '';
         };
       });
